8531ee8c21956e45dbc8a6ce42d9468252e0e0ab
Robsteranium <robin@robsteranium.plus.com>
2011-12-05 14:12:33 +0000

    comment validation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,12 @@
 class CommentsController < ApplicationController
   def create
       @post = Post.find(params[:post_id])
-      @comment = @post.comments.create(params[:comment])
-      redirect_to post_path(@post)
+      @comment = @post.comments.build(params[:comment])
+      if @comment.save
+        redirect_to post_path(@post), :notice => "Comment Posted, Thanks!"
+      else
+        render "posts/show", :error => "Couldn't save comment!"
+      end
   end
   
   def destroy
