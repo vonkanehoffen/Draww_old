8a7ccdd84a305db4ac61fc10190b50fc8dad5ca4
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2011-12-07 01:29:23 +0000

    Only display edit and delete links when logged in as post owning user

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,13 @@
 module PostsHelper
   def join_tags(post)
-      post.tags.map { |t| t.name }.join(", ")
+    post.tags.map { |t| t.name }.join(", ")
+  end
+    
+  def belongs_to_current_user(post)
+    if post.user == current_user 
+      return true
+    else
+      return false
     end
+  end
 end
