c86f73cfea0f8034f47ad024042aa889a33f6545
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2011-12-12 15:21:32 +0000

    a bit of markup and styling

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   
   attr_accessor :attachment64
   before_validation :save_attachment64
+  # TODO: needs to validate filesize
   
   # Paperclip
   # All photos should be 3 to 2 aspect
@@ -26,9 +27,22 @@ class Post < ActiveRecord::Base
   private
     def save_attachment64
       require Rails.root.join('lib', 'datafy.rb')
+      # filename = friendly_name(:title.to_s)
+      # puts "Friendly name = "+filename
+      # puts @post.title.inspect
+      # TODO: How do I get the post title here so I can make a filename out of it?
       File.open("tmp/reply.jpg", "wb") { |f| f.write(Datafy::decode_data_uri(attachment64)[0]) }  
       self.photo = File.open("tmp/reply.jpg", "r")
     end
 
+    def friendly_name(str)
+      s = Iconv.iconv('ascii//ignore//translit', 'utf-8', str).to_s
+      s.downcase!
+      s.gsub!(/'/, '')
+      s.gsub!(/[^A-Za-z0-9]+/, ' ')
+      s.strip!
+      s.gsub!(/\ +/, '-')
+      return s
+    end
 end
 
