a1329093200935529445cfedb47d2cd850655db1
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2011-12-29 13:55:43 +0000

    action bar styling + other bits

--- app/assets/javascripts/main_ui.js
+++ app/assets/javascripts/main_ui.js
@@ -5,12 +5,5 @@ $(document).ready(function() {
 	// In-place editing
 	$('.best_in_place').best_in_place();
 	
-	// Voting
-	// $('.post').hover(function() {
-	// 	$('.ranking',this).show();
-	// }, function() {
-	// 	$('.ranking',this).hide();
-	// })
-	
 });
 	
