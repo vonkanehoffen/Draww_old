73f3ec8b98cfa258484e951d5a9ef963e811f52d
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2012-02-12 14:22:40 +0000

    JS comment posting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,31 @@
 class CommentsController < ApplicationController
   before_filter :require_user
+  respond_to :html, :js
   def create
       @post = Post.find(params[:post_id])
       @comment = @post.comments.build(params[:comment])
       
-      respond_to do |format|
-        if @comment.save
-          current_user.comments << @comment
-          format.html { redirect_to post_path(@post), :notice => 'Comment Posted. Thanks!' }
-          # format.json { render :json => @post, :status => :created, :location => @post }
-        else
-          format.html { render "posts/show", :error => "Couldn't save the comment!" }
-          # format.json { render :json => @post.errors, :status => :unprocessable_entity }
-        end
+      if @comment.save
+        current_user.comments << @comment
+        flash[:notice] = 'Comment Posted. Thanks!'
+      else 
+        flash[:error] = 'Sorry, the comment could not be saved.'
       end
-            
+
+      respond_with(@post)
   end
   
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-    respond_to do |format|
-      if @comment.user == current_user
-        @comment.destroy
-        format.html { redirect_to post_path(@post), 
-          :notice => 'Your comment has been deleted.' }
-      else
-        # TODO: This should be an error. Howd I do that?
-        format.html { redirect_to post_path(@post), 
-          :notice => "Oi! That's not your comment to delete!" }
-      end     
+    
+    if @comment.user == current_user
+      @comment.destroy
+      flash[:notice] = 'Your comment has been deleted.'
+    else
+      flash[:error] = "Oi! That's not your comment to delete!"
     end
+    
+    respond_with(@post)
   end
 end
