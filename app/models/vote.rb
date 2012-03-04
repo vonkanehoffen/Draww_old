2d4603bc6f249a96737cce9ff43d2c79719d3916
vonkanehoffen <baron.vonkanehoffen@gmail.com>
2012-03-04 22:59:42 +0000

    a voting problem for robin

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,7 @@
 class Vote < ActiveRecord::Base
+  
+  # TODO: Here's a problem: A user creates an image. He automaticaly gets a vote (he'd do that anyway). Therefore casting power increases just by uplaoding stuff. Not good. Perhaps we need a validation saying you can't vote for your own post or something like that? Also, to stop gaming the system, wonder how hard it'd be to record the IP votes came from? Would be good data to have for the future....
+  
   belongs_to :user
   belongs_to :post
   
